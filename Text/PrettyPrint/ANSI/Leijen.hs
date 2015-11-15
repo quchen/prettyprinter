a1acb119f3abe2189789164e1220177dd787b3a0
Bradford Larsen <brad.larsen@gmail.com>
2015-11-14 22:35:44 -0500

    Add a documentation block for algebraic laws
    These laws came from Daan Leijen's original documentation
    (http://www.cs.uu.nl/~daan/download/pprint/pprint.html) which
    now appears to no longer be online.
    
    These laws are useful, so let's show them in the haddock documentation.

--- Text/PrettyPrint/ANSI/Leijen.hs
+++ Text/PrettyPrint/ANSI/Leijen.hs
@@ -75,6 +75,9 @@
 -- will only work on Unix-style operating systems.
 -----------------------------------------------------------
 module Text.PrettyPrint.ANSI.Leijen (
+   -- * The document algebra
+   -- $DocumentAlgebra
+
    -- * Documents
    Doc, putDoc, hPutDoc,
 
@@ -160,6 +163,70 @@ infixr 6 <>
 
 infixr 6 <+>
 infixr 5 </>,<//>,<$>,<$$>
+
+
+
+-- $DocumentAlgebra
+-- The combinators in this library satisfy many algebraic laws.
+--
+-- The concatenation operator '<>' is associative and has 'empty' as a left
+-- and right unit:
+--
+--     > x <> (y <> z)           = (x <> y) <> z
+--     > x <> empty              = x
+--     > empty <> x              = x
+--
+-- The 'text' combinator is a homomorphism from string concatenation to
+-- document concatenation:
+--
+--     > text (s ++ t)           = text s <> text t
+--     > text ""                 = empty
+--
+-- The 'char' combinator behaves like one-element text:
+--
+--     > char c                  = text [c]
+--
+-- The 'nest' combinator is a homomorphism from addition to document
+-- composition.  'nest' also distributes through document concatenation and is
+-- absorbed by 'text' and 'align':
+--
+--     > nest (i + j) x          = nest i (nest j x)
+--     > nest 0 x                = x
+--     > nest i (x <> y)         = nest i x <> nest i y
+--     > nest i empty            = empty
+--     > nest i (text s)         = text s
+--     > nest i (align x)        = align x
+--
+-- The 'group' combinator is absorbed by 'empty'.  'group' is commutative with
+-- 'nest' and 'align':
+--
+--     > group empty             = empty
+--     > group (text s <> x)     = text s <> group x
+--     > group (nest i x)        = nest i (group x)
+--     > group (align x)         = align (group x)
+--
+-- The 'align' combinator is absorbed by 'empty' and 'text'.
+-- 'align' is idempotent:
+--
+--     > align empty             = empty
+--     > align (text s)          = text s
+--     > align (align x)         = align x
+--
+-- From the laws of the primitive combinators, we can derive many other laws
+-- for the derived combinators.  For example, the /above/ operator '<$>' is
+-- defined as:
+--
+--     > x <$> y                 = x <> line <> y
+--
+-- It follows that '<$>' is associative and that '<$>' and '<>' associate
+-- with each other:
+--
+--     > x <$> (y <$> z)         = (x <$> y) <$> z
+--     > x <> (y <$> z)          = (x <> y) <$> z
+--     > x <$> (y <> z)          = (x <$> y) <> z
+--
+-- Similar laws also hold for the other line break operators '</>', '<$$>',
+-- and '<//>'.
 
 
 -----------------------------------------------------------
