f996a5d4405e25b065eb6072707fc4cdca3ab6a1
David Luposchainsky <dluposchainsky@gmail.com>
2016-11-27 16:47:21 +0100

    Add marked text rendering

--- src/Data/Text/PrettyPrint/Doc/Display/Text.hs
+++ src/Data/Text/PrettyPrint/Doc/Display/Text.hs
@@ -5,6 +5,9 @@
 module Data.Text.PrettyPrint.Doc.Display.Text (
     displayLazyText,
     displayStrictText,
+
+    displayMarkedLazyText,
+    displayMarkedStrictText,
 ) where
 
 import           Data.Monoid
@@ -20,6 +23,8 @@ import Data.Text.PrettyPrint.Doc
 -- $setup
 -- >>> :set -XOverloadedStrings
 -- >>> :set -XLambdaCase
+-- >>> import qualified Data.Text.IO as T
+-- >>> import qualified Data.Text.Lazy.IO as LT
 
 
 
@@ -44,3 +49,36 @@ displayLazyText = LTB.toLazyText . build
 -- transforms it to strict text.
 displayStrictText :: SimpleDoc -> Text
 displayStrictText = LT.toStrict . displayLazyText
+
+-- | Add subtle markers for emphasis, strong emphasis and underlining. Note that
+-- this happens after the rendering algorithm is done, so the added characters
+-- may result in lines slighly longer than the page width.
+--
+-- >>> let doc = "This text" <+> bold ("is bold" <+> italics "with emphasis" <+> "!")
+-- >>> let pprint = LT.putStrLn . displayMarkedLazyText . renderPretty 0.4 40
+-- >>> pprint doc
+-- This text **is bold *with emphasis* !**
+displayMarkedLazyText :: SimpleDoc -> LT.Text
+displayMarkedLazyText = LTB.toLazyText . build
+  where
+    build = \case
+        SFail        -> error "@SFail@ can not appear uncaught in a rendered @SimpleDoc@"
+        SEmpty       -> mempty
+        SChar c x    -> LTB.singleton c <> build x
+        SText t x    -> LTB.fromText t <> build x
+        SLine i x    -> LTB.singleton '\n' <> LTB.fromText (T.replicate i " ") <> build x
+        SStyle s x y -> applyStyle s x <> build y
+
+    applyStyle :: Style -> SimpleDoc -> LTB.Builder
+    applyStyle s d = case s of
+        SItalicized  -> encloseIn "*"  (build d)
+        SBold        -> encloseIn "**" (build d)
+        SUnderlined  -> encloseIn "_"  (build d)
+        SColor _ _ _ -> build d -- We cannot colour text :-(
+
+encloseIn :: Monoid m => m -> m -> m
+encloseIn e x = e <> x <> e
+
+-- | Strict version of 'displayMarkedLazyText'.
+displayMarkedStrictText :: SimpleDoc -> Text
+displayMarkedStrictText = LT.toStrict . displayLazyText
