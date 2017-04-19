64d9440610397cf8f5720b86e3b7c245b491f821
David Luposchainsky <dluposchainsky@gmail.com>
2017-04-19 18:23:10 +0200

    Reinstate non-internal Doc module as a reexport of (most of) the internals

---
+++ wl-pprint/src/Data/Text/Prettyprint/Doc.hs
@@ -0,0 +1,328 @@
+-- |
+-- Module      :  Data.Text.Prettyprint.Doc
+-- Copyright   :  Daan Leijen (c) 2000, http://www.cs.uu.nl/~daan
+--                Max Bolingbroke (c) 2008, http://blog.omega-prime.co.uk
+--                David Luposchainsky (c) 2016, http://github.com/quchen
+-- License     :  BSD-style (see the file LICENSE.md)
+-- Maintainer  :  David Luposchainsky <dluposchainsky (λ) google>
+-- Stability   :  experimental
+-- Portability :  portable
+--
+-- = Overview
+--
+-- This module defines a prettyprinter to format text in a flexible and
+-- convenient way. The idea is to combine a 'Doc'ument out of many small
+-- components, then using a layouter to convert it to an easily renderable
+-- 'SimpleDoc', which can then be rendered to a variety of formats, for example
+-- plain 'Text'.
+--
+-- The documentation consists of several parts:
+--
+--   1. Just below is some general textual information about the library.
+--   2. The actual library with extensive documentation and examples
+--   3. Migration guide for users familiar with (ansi-)wl-pprint
+--   4. Historical notes about previous libraries
+--   5. Algebraic properties
+--
+-- = How the layout works
+--
+-- There are two key concepts to laying a document out: the available width, and
+-- 'group'ing.
+--
+-- == Available width
+--
+-- The page has a certain maximum width, which the layouter tries to not exceed,
+-- by inserting line breaks where possible. The functions given in this module
+-- make it fairly straightforward to specify where, and under what
+-- circumstances, such a line break may be inserted by the layouter, for example
+-- via the 'sep' function.
+--
+-- There is also the concept of /ribbon width/. The ribbon is the part of a line
+-- that is printed, i.e. the line length without the leading indentation. The
+-- layouters take a ribbon fraction argument, which specifies how much of a line
+-- should be filled before trying to break it up. A ribbon width of 0.5 in a
+-- document of width 80 will result in the layouter to try to not exceed @0.5*80 =
+-- 40@ (ignoring current indentation depth).
+--
+-- == Grouping
+--
+-- A document can be 'group'ed, which tells the layouter that it should attempt
+-- to collapse it to a single line. If the result does not fit within the
+-- constraints (given by page and ribbon widths), the document is rendered
+-- unaltered. This allows fallback definitions, so that we get nice results even
+-- when the original document would exceed the layout constraints.
+--
+-- == Starting out
+--
+-- As a reading list for starters, some of the most commonly used functions in
+-- this module include '<>', 'hsep', '<+>', 'vsep', 'align', 'hang'. These cover
+-- many use cases already, and many other functions are variations or
+-- combinations of these.
+--
+-- = Example
+--
+-- The layout of the document can adapt to the available space. For example, we
+-- can define a prettyprinter for simple type declaration that aligns over
+-- multiple lines nicely if space does not permit it to fit in a single line.
+--
+-- Let's look at some code for doing this:
+--
+-- >>> let prettyType = align . sep . zipWith (<+>) ("::" : repeat "->")
+-- >>> let prettyDecl n tys = pretty n <+> prettyType tys
+-- >>> let doc = prettyDecl "example" ["Int", "Bool", "Char", "IO ()"]
+--
+-- If the page is wide enough (80 characters in this case), the definitions are
+-- space-separated,
+--
+-- >>> putDocW 80 doc
+-- example :: Int -> Bool -> Char -> IO ()
+--
+-- If we narrow the page width to only 20 characters, the /same document/
+-- renders vertically aligned:
+--
+-- >>> putDocW 20 doc
+-- example :: Int
+--         -> Bool
+--         -> Char
+--         -> IO ()
+module Data.Text.Prettyprint.Doc (
+    -- * Documents
+    Doc,
+
+    -- * Basic functionality
+    Pretty(..),
+    emptyDoc, nest, line, line', softline, softline', hardline, group, flatAlt,
+
+    -- * Alignment functions
+    --
+    -- | The functions in this section cannot be described by Wadler's original
+    -- functions. They align their output relative to the current output
+    -- position - in contrast to @'nest'@ which always aligns to the current
+    -- nesting level. This deprives these functions from being \'optimal\'. In
+    -- practice however they prove to be very useful. The functions in this
+    -- section should be used with care, since they are more expensive than the
+    -- other functions. For example, @'align'@ shouldn't be used to pretty print
+    -- all top-level declarations of a language, but using @'hang'@ for let
+    -- expressions is fine.
+    align, hang, indent, encloseSep, list, tupled,
+
+    -- * Binary functions
+    (<>), (<+>),
+
+    -- * List functions
+
+    -- | The 'sep' and 'cat' functions differ in one detail: when 'group'ed, the
+    -- 'sep's replace newlines wich 'space's, while the 'cat's simply remove
+    -- them. If you're not sure what you want, start with the 'sep's.
+
+    concatWith,
+
+    -- ** 'sep' family
+    --
+    -- | When 'group'ed, these will replace newlines with spaces.
+    hsep, vsep, fillSep, sep,
+    -- ** 'cat' family
+    --
+    -- | When 'group'ed, these will remove newlines.
+    hcat, vcat, fillCat, cat,
+    -- ** Others
+    punctuate,
+
+    -- * Reactive/conditional layouts
+    --
+    -- | Lay documents out differently based on current position and the page
+    -- layout.
+    column, nesting, width, pageWidth,
+
+    -- * Filler functions
+    --
+    -- | Fill up available space
+    fill, fillBreak,
+
+    -- * General convenience
+    --
+    -- | Useful helper functions.
+    plural, enclose,
+
+    -- * Bracketing functions
+    --
+    -- | Enclose documents in common ways.
+    squotes, dquotes, parens, angles, brackets, braces,
+
+    -- * Named characters
+    --
+    -- | Convenience definitions for common characters
+    lparen, rparen, langle, rangle, lbrace, rbrace, lbracket, rbracket, squote,
+    dquote, semi, colon, comma, space, dot, slash, backslash, equals, pipe,
+
+    -- * Styling
+
+    Style(..), SColor(..), SIntensity(..), SLayer(..),
+
+    -- ** Font color
+    color, colorDull,
+    --
+    -- ** Background color
+    bgColor, bgColorDull,
+
+    -- ** Font style
+    bold, italics, underline,
+
+    -- ** Remove formatting
+    plain,
+
+    -- * Optimization
+    --
+    -- Render documents faster
+    fuse, FusionDepth(..),
+
+    -- * Layout
+    --
+    -- | Laying a 'Doc'ument out produces a straightforward 'SimpleDoc' based on
+    -- parameters such as page width and ribbon size, by evaluating how a 'Doc'
+    -- fits these constraints the best.
+    SimpleDoc(..),
+    PageWidth(..), LayoutOptions(..), defaultLayoutOptions,
+    layoutPretty, layoutCompact, layoutSmart,
+
+    -- * Notes
+
+    -- ** Migration guide
+    --
+    -- $migration
+
+    -- ** Historical
+    --
+    -- $history
+
+    -- ** Algebraic properties
+    --
+    -- $algebra
+
+) where
+
+
+
+import Data.Monoid
+import Data.Text.Prettyprint.Doc.Internal
+
+-- $setup
+-- >>> :set -XOverloadedStrings
+-- >>> import Data.Text.Prettyprint.Doc.Render.Text
+-- >>> let layoutOptions w = LayoutOptions { layoutRibbonFraction = 1, layoutPageWidth = CharsPerLine w }
+-- >>> let putDocW w doc = renderIO System.IO.stdout (layoutPretty (layoutOptions w) doc)
+
+
+
+-- $migration
+--
+-- If you're already familiar with (ansi-)wl-pprint, you'll recognize many
+-- functions in this module, and they work just the same way. However, a couple
+-- of definitions are missing:
+--
+--   - @char@, @string@, @double@, … – these are all special cases of the
+--     overloaded @'pretty'@ function.
+--   - @\<$>@, @\<$$>@, @\</>@, @\<//>@ are special cases of
+--     @'vsep'@, @'vcat'@, @'fillSep'@, @'fillCat'@ with only two documents.
+--   - If you need 'String' output, use 'T.unpack' on the generated renderings.
+--   - The /display/ functions are moved to the rendering submodules.
+--   - The /render/ functions are called /layout/ functions.
+--   - Instead of providing an own colorization function for each
+--     color\/intensity\/layer combination, they have been combined in 'color',
+--     'colorDull', 'bgColor', and 'bgColorDull' functions.
+
+
+
+-- $history
+--
+-- This module is based on previous work by Daan Leijen and Max Bolingbroke, who
+-- implemented and significantly extended the prettyprinter given by a paper by
+-- Phil Wadler in his 1997 paper "A Prettier Printer", by adding lots of
+-- convenience functions, styling, and new functionality. Their package,
+-- <http:/hackage.haskell.org/package/ansi-wl-pprint ansi-wl-pprint> is widely
+-- used in the Haskell ecosystem.
+--
+-- However, ansi-wl-pprint is showing its age, resulting in a couple of issues:
+--
+--   - Definitions clashing with others that are now standard Haskell, such as
+--     @\<$>@
+--   - Hard to read operators, such as @\<//>@
+--   - Some undocumented definitions, not many examples
+--   - Based on 'String'
+--
+-- This modified package addresses and modernizes these issues:
+--
+--   - No clashing definitions
+--   - All but the essential @'<>'@ and @'<+>'@ operators removed
+--   - Everything extensively documented, with references to other functions and
+--     runnable code examples
+--   - 'Text' instead of 'String'
+
+
+
+-- $algebra
+--
+-- The functions in this library satisfy many algebraic laws.
+--
+-- The 'text' function is a homomorphism from text concatenation to document
+-- concatenation:
+--
+-- @
+-- 'text' (s '<>' t) = 'text' s '<>' 'text' t
+-- 'text' 'mempty' = 'mempty'
+-- @
+--
+-- The 'char' function behaves like one-element text:
+--
+-- @
+-- 'char' c = 'text' ('T.singleton' c)
+-- @
+--
+-- The 'nest' function is a homomorphism from addition to document composition.
+-- 'nest' also distributes through document concatenation and is absorbed by
+-- 'text' (without newlines) and 'align':
+--
+-- @
+-- 'nest' (i '+' j) x = 'nest' i ('nest' j x)
+-- 'nest' 0 x = x
+-- 'nest' i (x '<>' y) = 'nest' i x '<>' 'nest' i y
+-- 'nest' i 'mempty' = 'mempty'
+-- 'nest' i ('text' t) = 'text' t -- no newline in t
+-- 'nest' i ('align' x) = 'align' x
+-- @
+--
+-- The 'group' function is absorbed by 'mempty'. 'group' is commutative with
+-- 'nest' and 'align':
+--
+-- @
+-- 'group' 'mempty' = 'mempty'
+-- 'group' ('text' s '<>' x) = 'text' s '<>' 'group' x
+-- 'group' ('nest' i x) = 'nest' i ('group' x)
+-- 'group' ('align' x) = 'align' ('group' x)
+-- @
+--
+-- The 'align' function is absorbed by 'mempty' and 'text'. 'align' is
+-- idempotent:
+--
+-- @
+-- 'align' 'mempty' = 'mempty'
+-- 'align' ('text' s) = 'text' s
+-- 'align' ('align' x) = 'align' x
+-- @
+--
+-- From the laws of the primitive functions, we can derive many other laws for
+-- the derived functions. For example, the /spaced/ operator '<+>' is defined
+-- as:
+--
+-- @
+-- x '<+>' y = x '<>' 'space' '<>' y
+-- @
+--
+-- It follows that '<+>' is associative and that '<+>' and '<>' associate with
+-- each other:
+--
+-- @
+-- x '<+>' (y '<+>' z) = (x '<+>' y) '<+>' z
+-- x '<>' (y '<+>' z) = (x '<>' y) '<+>' z
+-- x '<+>' (y <> z) = (x '<+>' y) '<>' z
+-- @
--- wl-pprint/src/Data/Text/Prettyprint/Doc/Internal.hs
+++ wl-pprint/src/Data/Text/Prettyprint/Doc/Internal.hs
@@ -3,207 +3,15 @@
 {-# LANGUAGE LambdaCase        #-}
 {-# LANGUAGE OverloadedStrings #-}
 
--- |
--- Module      :  Data.Text.Prettyprint.Doc
--- Copyright   :  Daan Leijen (c) 2000, http://www.cs.uu.nl/~daan
---                Max Bolingbroke (c) 2008, http://blog.omega-prime.co.uk
---                David Luposchainsky (c) 2016, http://github.com/quchen
--- License     :  BSD-style (see the file LICENSE.md)
--- Maintainer  :  David Luposchainsky <dluposchainsky (λ) google>
--- Stability   :  experimental
--- Portability :  portable
+-- | __Warning: internal module!__ This means that the API may change
+-- arbitrarily between versions without notice. Depending on this module may
+-- lead to unexpected breakages, so proceed with caution!
 --
--- = Overview
+-- For a stable API, use the non-internal modules.
 --
--- This module defines a prettyprinter to format text in a flexible and
--- convenient way. The idea is to combine a 'Doc'ument out of many small
--- components, then using a layouter to convert it to an easily renderable
--- 'SimpleDoc', which can then be rendered to a variety of formats, for example
--- plain 'Text'.
---
--- The documentation consists of several parts:
---
---   1. Just below is some general textual information about the library.
---   2. The actual library with extensive documentation and examples
---   3. Migration guide for users familiar with (ansi-)wl-pprint
---   4. Historical notes about previous libraries
---   5. Algebraic properties
---
--- = How the layout works
---
--- There are two key concepts to laying a document out: the available width, and
--- 'group'ing.
---
--- == Available width
---
--- The page has a certain maximum width, which the layouter tries to not exceed,
--- by inserting line breaks where possible. The functions given in this module
--- make it fairly straightforward to specify where, and under what
--- circumstances, such a line break may be inserted by the layouter, for example
--- via the 'sep' function.
---
--- There is also the concept of /ribbon width/. The ribbon is the part of a line
--- that is printed, i.e. the line length without the leading indentation. The
--- layouters take a ribbon fraction argument, which specifies how much of a line
--- should be filled before trying to break it up. A ribbon width of 0.5 in a
--- document of width 80 will result in the layouter to try to not exceed @0.5*80 =
--- 40@ (ignoring current indentation depth).
---
--- == Grouping
---
--- A document can be 'group'ed, which tells the layouter that it should attempt
--- to collapse it to a single line. If the result does not fit within the
--- constraints (given by page and ribbon widths), the document is rendered
--- unaltered. This allows fallback definitions, so that we get nice results even
--- when the original document would exceed the layout constraints.
---
--- == Starting out
---
--- As a reading list for starters, some of the most commonly used functions in
--- this module include '<>', 'hsep', '<+>', 'vsep', 'align', 'hang'. These cover
--- many use cases already, and many other functions are variations or
--- combinations of these.
---
--- = Example
---
--- The layout of the document can adapt to the available space. For example, we
--- can define a prettyprinter for simple type declaration that aligns over
--- multiple lines nicely if space does not permit it to fit in a single line.
--- Let's look at some code for doing this:
---
--- >>> let prettyType = align . sep . zipWith (<+>) ("::" : repeat "->")
--- >>> let prettyDecl n tys = pretty n <+> prettyType tys
--- >>> let doc = prettyDecl "example" ["Int", "Bool", "Char", "IO ()"]
---
--- If the page is wide enough (80 characters in this case), the definitions are
--- space-separated,
---
--- >>> putDocW 80 doc
--- example :: Int -> Bool -> Char -> IO ()
---
--- If we narrow the page width to only 20 characters, the /same document/
--- renders vertically aligned:
---
--- >>> putDocW 20 doc
--- example :: Int
---         -> Bool
---         -> Char
---         -> IO ()
-module Data.Text.Prettyprint.Doc (
-    -- * Documents
-    Doc,
-
-    -- * Basic functionality
-    Pretty(..),
-    emptyDoc, nest, line, line', softline, softline', hardline, group, flatAlt,
-
-    -- * Alignment functions
-    --
-    -- | The functions in this section cannot be described by Wadler's original
-    -- functions. They align their output relative to the current output
-    -- position - in contrast to @'nest'@ which always aligns to the current
-    -- nesting level. This deprives these functions from being \'optimal\'. In
-    -- practice however they prove to be very useful. The functions in this
-    -- section should be used with care, since they are more expensive than the
-    -- other functions. For example, @'align'@ shouldn't be used to pretty print
-    -- all top-level declarations of a language, but using @'hang'@ for let
-    -- expressions is fine.
-    align, hang, indent, encloseSep, list, tupled,
-
-    -- * Binary functions
-    (<>), (<+>),
-
-    -- * List functions
-
-    -- | The 'sep' and 'cat' functions differ in one detail: when 'group'ed, the
-    -- 'sep's replace newlines wich 'space's, while the 'cat's simply remove
-    -- them. If you're not sure what you want, start with the 'sep's.
-
-    concatWith,
-
-    -- ** 'sep' family
-    --
-    -- | When 'group'ed, these will replace newlines with spaces.
-    hsep, vsep, fillSep, sep,
-    -- ** 'cat' family
-    --
-    -- | When 'group'ed, these will remove newlines.
-    hcat, vcat, fillCat, cat,
-    -- ** Others
-    punctuate,
-
-    -- * Reactive/conditional layouts
-    --
-    -- | Lay documents out differently based on current position and the page
-    -- layout.
-    column, nesting, width, pageWidth,
-
-    -- * Filler functions
-    --
-    -- | Fill up available space
-    fill, fillBreak,
-
-    -- * General convenience
-    --
-    -- | Useful helper functions.
-    plural, enclose,
-
-    -- * Bracketing functions
-    --
-    -- | Enclose documents in common ways.
-    squotes, dquotes, parens, angles, brackets, braces,
-
-    -- * Named characters
-    --
-    -- | Convenience definitions for common characters
-    lparen, rparen, langle, rangle, lbrace, rbrace, lbracket, rbracket, squote,
-    dquote, semi, colon, comma, space, dot, slash, backslash, equals, pipe,
-
-    -- * Styling
-
-    Style(..), SColor(..), SIntensity(..), SLayer(..),
-
-    -- ** Font color
-    color, colorDull,
-    --
-    -- ** Background color
-    bgColor, bgColorDull,
-
-    -- ** Font style
-    bold, italics, underline,
-
-    -- ** Remove formatting
-    plain,
-
-    -- * Optimization
-    --
-    -- Render documents faster
-    fuse, FusionDepth(..),
-
-    -- * Layout
-    --
-    -- | Laying a 'Doc'ument out produces a straightforward 'SimpleDoc' based on
-    -- parameters such as page width and ribbon size, by evaluating how a 'Doc'
-    -- fits these constraints the best.
-    SimpleDoc(..),
-    PageWidth(..), LayoutOptions(..), defaultLayoutOptions,
-    layoutPretty, layoutCompact, layoutSmart,
-
-    -- * Notes
-
-    -- ** Migration guide
-    --
-    -- $migration
-
-    -- ** Historical
-    --
-    -- $history
-
-    -- ** Algebraic properties
-    --
-    -- $algebra
-
-) where
+-- The purpose of this module is mostly fully exposing the 'Doc' type so other
+-- libraries can write adaptors to and from this library.
+module Data.Text.Prettyprint.Doc.Internal where
 
 
 
@@ -262,25 +70,26 @@ data Doc =
     -- choosing a more suitable rendering.
     Fail
 
-    -- | The empty document; unit of Cat (observationally)
+    -- | The empty document; unit of 'Cat' (observationally)
     | Empty
 
     -- | invariant: char is not '\n'
     | Char Char
 
-    -- | invariants: at least two characters, doesn't contain '\n'. For empty
-    -- documents, there is @Empty@; for singleton documents, there is @Char@;
-    -- newlines should be replaced by e.g. @Line@.
+    -- | Invariants: at least two characters long, does not contain '\n'. For
+    -- empty documents, there is @Empty@; for singleton documents, there is
+    -- @Char@; newlines should be replaced by e.g. @Line@.
     --
-    -- Since the frequently used 'T.length' of 'Text' is /O(n)/, we cache it
-    -- in this constructor.
+    -- Since the frequently used 'T.length' of 'Text' is /O(length)/, we cache
+    -- it in this constructor.
     | Text {-# UNPACK #-} !Int Text
 
     -- | Line break
     | Line
 
     -- | Lay out the first 'Doc', but when flattened (via 'group'), fall back to
-    -- the second.
+    -- the second. The flattened version should in general be higher and
+    -- narrower than the fallback.
     | FlatAlt Doc Doc
 
     -- | Concatenation of two documents
@@ -289,7 +98,8 @@ data Doc =
     -- | Document indented by a number of columns
     | Nest !Int Doc
 
-    -- | Invariant: first lines of first 'Doc' longer than the first lines of the second one
+    -- | Invariant: first lines of first 'Doc' longer than the first lines of
+    -- the second one. Used to implement layout alternatives for 'group'.
     | Union Doc Doc
 
     -- | React on the current cursor position, see 'column'
@@ -314,6 +124,10 @@ instance Monoid Doc where
     mappend = (Semi.<>)
     mconcat = hcat
 
+-- |
+-- @
+-- ('Semi.<>') = 'hcat'
+-- @
 instance Semi.Semigroup Doc where
     (<>) = Cat
 
@@ -1598,7 +1412,7 @@ defaultLayoutOptions = LayoutOptions
     , layoutRibbonFraction = 0.4
     }
 
--- | This is the default pretty printer which is used by 'show', 'putDoc' and
+-- | This is the default prettyprinter which is used by 'show', 'putDoc' and
 -- 'hPutDoc'. @(layoutPretty ribbonfrac width x)@ lays out the document @x@ with
 -- a page width of @width@ and a ribbon width of @(ribbonfrac * width)@
 -- characters. The ribbon width is the maximal amount of non-indentation
@@ -1772,7 +1586,7 @@ layoutFits
 
 -- | @(layoutCompact x)@ lays out the document @x@ without adding any
 -- indentation. Since no \'pretty\' printing is involved, this layouter is very
--- fast. The resulting output contains fewer characters than a pretty printed
+-- fast. The resulting output contains fewer characters than a prettyprinted
 -- version and can be used for output that is read by other programs.
 --
 -- This layout function does not add any colorisation information.
@@ -1809,8 +1623,7 @@ layoutCompact doc = scan 0 [doc]
         Nesting f       -> scan k (f 0:ds)
         StylePush _ x   -> scan k (x:ds)
 
--- | @('show' doc)@ pretty prints document @doc@ with a page width of 80
--- characters and a ribbon width of 32 characters.
+-- | @('show' doc)@ prettyprints document @doc@ with 'defaultLayoutOptions'.
 instance Show Doc where
     showsPrec _ doc = displayString (layoutPretty defaultLayoutOptions doc)
 
@@ -1823,118 +1636,3 @@ displayString = \case
     SLine i x      -> showString ('\n':replicate i ' ') . displayString x
     SStylePush _ x -> displayString x
     SStylePop x    -> displayString x
-
-
-
--- $migration
---
--- If you're already familiar with (ansi-)wl-pprint, you'll recognize many
--- functions in this module, and they work just the same way. However, a couple
--- of definitions are missing:
---
---   - @char@, @string@, @double@, … – these are all special cases of the
---     overloaded @'pretty'@ function.
---   - @\<$>@, @\<$$>@, @\</>@, @\<//>@ are special cases of
---     @'vsep'@, @'vcat'@, @'fillSep'@, @'fillCat'@ with only two documents.
---   - If you need 'String' output, use 'T.unpack' on the generated renderings.
---   - The /display/ functions are moved to the rendering submodules.
---   - The /render/ functions are called /layout/ functions.
---   - Instead of providing an own colorization function for each
---     color\/intensity\/layer combination, they have been combined in 'color',
---     'colorDull', 'bgColor', and 'bgColorDull' functions.
-
-
-
--- $history
---
--- This module is based on previous work by Daan Leijen and Max Bolingbroke, who
--- implemented and significantly extended the prettyprinter given by a paper by
--- Phil Wadler in his 1997 paper "A Prettier Printer", by adding lots of
--- convenience functions, styling, and new functionality. Their package,
--- <http:/hackage.haskell.org/package/ansi-wl-pprint ansi-wl-pprint> is widely
--- used in the Haskell ecosystem.
---
--- However, ansi-wl-pprint is showing its age, resulting in a couple of issues:
---
---   - Definitions clashing with others that are now standard Haskell, such as
---     @\<$>@
---   - Hard to read operators, such as @\<//>@
---   - Some undocumented definitions, not many examples
---   - Based on 'String'
---
--- This modified package addresses and modernizes these issues:
---
---   - No clashing definitions
---   - All but the essential @'<>'@ and @'<+>'@ operators removed
---   - Everything extensively documented, with references to other functions and
---     runnable code examples
---   - 'Text' instead of 'String'
-
-
-
--- $algebra
---
--- The functions in this library satisfy many algebraic laws.
---
--- The 'text' function is a homomorphism from text concatenation to document
--- concatenation:
---
--- @
--- 'text' (s '<>' t) = 'text' s '<>' 'text' t
--- 'text' 'mempty' = 'mempty'
--- @
---
--- The 'char' function behaves like one-element text:
---
--- @
--- 'char' c = 'text' ('T.singleton' c)
--- @
---
--- The 'nest' function is a homomorphism from addition to document composition.
--- 'nest' also distributes through document concatenation and is absorbed by
--- 'text' (without newlines) and 'align':
---
--- @
--- 'nest' (i '+' j) x = 'nest' i ('nest' j x)
--- 'nest' 0 x = x
--- 'nest' i (x '<>' y) = 'nest' i x '<>' 'nest' i y
--- 'nest' i 'mempty' = 'mempty'
--- 'nest' i ('text' t) = 'text' t -- no newline in t
--- 'nest' i ('align' x) = 'align' x
--- @
---
--- The 'group' function is absorbed by 'mempty'. 'group' is commutative with
--- 'nest' and 'align':
---
--- @
--- 'group' 'mempty' = 'mempty'
--- 'group' ('text' s '<>' x) = 'text' s '<>' 'group' x
--- 'group' ('nest' i x) = 'nest' i ('group' x)
--- 'group' ('align' x) = 'align' ('group' x)
--- @
---
--- The 'align' function is absorbed by 'mempty' and 'text'. 'align' is
--- idempotent:
---
--- @
--- 'align' 'mempty' = 'mempty'
--- 'align' ('text' s) = 'text' s
--- 'align' ('align' x) = 'align' x
--- @
---
--- From the laws of the primitive functions, we can derive many other laws for
--- the derived functions. For example, the /spaced/ operator '<+>' is defined
--- as:
---
--- @
--- x '<+>' y = x '<>' 'space' '<>' y
--- @
---
--- It follows that '<+>' is associative and that '<+>' and '<>' associate with
--- each other:
---
--- @
--- x '<+>' (y '<+>' z) = (x '<+>' y) '<+>' z
--- x '<>' (y '<+>' z) = (x '<>' y) '<+>' z
--- x '<+>' (y <> z) = (x '<+>' y) '<>' z
--- @
