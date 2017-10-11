dba8a03bf3320dd0115091c487430f7e76e8af15
Rob Rix <rob.rix@me.com>
2017-10-11 11:55:46 -0400

    Split up the instances over multiple lines.

--- prettyprinter/src/Data/Text/Prettyprint/Doc/Internal.hs
+++ prettyprinter/src/Data/Text/Prettyprint/Doc/Internal.hs
@@ -393,10 +393,18 @@ instance Pretty Void where pretty = absurd
 --
 -- >>> data Expr a = Plus a a | Times a a | Const Int
 -- >>> newtype Fix f = Fix (f (Fix f))
--- >>>
--- >>> instance Pretty1 Expr where liftPretty p e = case e of { Plus a b -> parens (p a <+> pretty '+' <+> p b) ; Times a b -> p a <+> pretty '*' <+> p b ; Const i -> pretty i }
--- >>> instance Pretty1 f => Pretty (Fix f) where pretty (Fix f) = liftPretty pretty f
--- >>>
+-- >>> :{
+-- >>> instance Pretty1 Expr where
+-- >>>   liftPretty p (Plus  a b) = parens (p a <+> pretty '+' <+> p b)
+-- >>>   liftPretty p (Times a b) =         p a <+> pretty '*' <+> p b
+-- >>>   liftPretty _ (Const i)   = pretty i
+-- >>> :}
+--
+-- >>> :{
+-- >>> instance Pretty1 f => Pretty (Fix f) where
+-- >>>   pretty (Fix f) = liftPretty pretty f
+-- >>> :}
+--
 -- >>> pretty (Fix (Times (Fix (Plus (Fix (Const 1)) (Fix (Const 2)))) (Fix (Const 3))))
 -- (1 + 2) * 3
 class Pretty1 f where
