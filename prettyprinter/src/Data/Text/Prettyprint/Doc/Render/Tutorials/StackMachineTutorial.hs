d4cffab6defb0059d932515e02109400f8b17808
Simon Jakobi <simon.jakobi@gmail.com>
2020-07-04 23:32:35 +0200

    Add explanation

--- prettyprinter/src/Data/Text/Prettyprint/Doc/Render/Tutorials/StackMachineTutorial.hs
+++ prettyprinter/src/Data/Text/Prettyprint/Doc/Render/Tutorials/StackMachineTutorial.hs
@@ -6,4 +6,6 @@ module Data.Text.Prettyprint.Doc.Render.Tutorials.StackMachineTutorial (
     module Prettyprinter.Render.Tutorials.StackMachineTutorial
 ) where
 
+-- Yeah, this produces a deprecation warning. It's hard to disable it while
+-- staying compatible with GHC < 8.0 though. Don't waste your time.
 import Prettyprinter.Render.Tutorials.StackMachineTutorial
