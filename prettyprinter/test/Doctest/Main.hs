c7e4a4bde7d0489f6db28f83a739aa39347f58cb
Simon Jakobi <simon.jakobi@gmail.com>
2025-11-29 16:29:06 +0100

    Silence `-Wunused-packages`

--- prettyprinter/test/Doctest/Main.hs
+++ prettyprinter/test/Doctest/Main.hs
@@ -2,5 +2,9 @@ module Main (main) where
 
 import Test.DocTest
 
+-- Silence -Wunused-packages:
+import Prettyprinter   ()
+import Test.QuickCheck ()
+
 main :: IO ()
 main = doctest [ "src" , "-Imisc"]
